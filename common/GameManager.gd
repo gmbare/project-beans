c5fd131161094aaef1b9f1d2c3a32e1a954a97fa
Garett Bare <Garett.bare@gmail.com>
2026-03-19 01:37:01 -0500

    making the beans bounce

--- common/GameManager.gd
+++ common/GameManager.gd
@@ -1,1 +1,6 @@
 extends Node
+
+var win_size : Vector2
+
+func _ready() -> void:
+	win_size = DisplayServer.window_get_size()
---
+++ lib/scenes/bean/bean_spawner.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var bean_uid : String
+var bean_scene : PackedScene
+
+
+func _ready() -> void:
+	bean_scene = load(bean_uid)
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		var loaded_scene := bean_scene.instantiate()
+		add_child(loaded_scene)
+		loaded_scene.position = Vector2(randf_range(0,GM.win_size.x), -5)
