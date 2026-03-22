af095e8448b757290826ef833ed5b0d8ac8ad64c
Garett Bare <Garett.bare@gmail.com>
2026-03-22 08:29:09 -0500

    setting up bounds to let the eye watch the beans

---
+++ lib/scenes/bean_eater/eyes.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name eyes
+
+@export var left_pupil : Sprite2D
+@export var right_pupil : Sprite2D
+
+var upper_bounds : Vector2 = Vector2(-31,-31)
+var lower_bounds : Vector2 = Vector2(6,6)
+
+var left_bounds : Vector2 = Vector2(-66,205)
+var right_bounds : Vector2 = Vector2(-180,94)
